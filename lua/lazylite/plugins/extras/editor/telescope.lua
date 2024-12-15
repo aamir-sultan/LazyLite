5ab3329df01e5cae1ce6f9cdb5f6d1eb5f382833
aamir-sultan <asultan.here@gmail.com>
2024-12-15 22:06:50 +0500

    chore: updated telescope settings for the LazyLite

--- lua/lazylite/plugins/extras/editor/telescope.lua
+++ lua/lazylite/plugins/extras/editor/telescope.lua
@@ -69,7 +69,8 @@ return {
       {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = (build_cmd ~= "cmake") and "make"
-          or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+            or
+            "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
         enabled = build_cmd ~= nil,
         config = function(plugin)
           LazyLite.on_load("telescope.nvim", function()
@@ -95,49 +96,49 @@ return {
         "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
         desc = "Switch Buffer",
       },
-      { "<leader>/", LazyLite.pick("live_grep"), desc = "Grep (Root Dir)" },
-      { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-      { "<leader><space>", LazyLite.pick("files"), desc = "Find Files (Root Dir)" },
+      { "<leader>/",       LazyLite.pick("live_grep"),           desc = "Grep (Root Dir)" },
+      { "<leader>:",       "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      { "<leader><space>", LazyLite.pick("files"),               desc = "Find Files (Root Dir)" },
       -- find
       {
         "<leader>fb",
         "<cmd>Telescope buffers sort_mru=true sort_lastused=true ignore_current_buffer=true<cr>",
         desc = "Buffers",
       },
-      { "<leader>fc", LazyLite.pick.config_files(), desc = "Find Config File" },
-      { "<leader>ff", LazyLite.pick("files"), desc = "Find Files (Root Dir)" },
-      { "<leader>fF", LazyLite.pick("files", { root = false }), desc = "Find Files (cwd)" },
-      { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
-      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-      { "<leader>fR", LazyLite.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
+      { "<leader>fc", LazyLite.pick.config_files(),                                      desc = "Find Config File" },
+      { "<leader>ff", LazyLite.pick("files"),                                            desc = "Find Files (Root Dir)" },
+      { "<leader>fF", LazyLite.pick("files", { root = false }),                          desc = "Find Files (cwd)" },
+      { "<leader>fg", "<cmd>Telescope git_files<cr>",                                    desc = "Find Files (git-files)" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>",                                     desc = "Recent" },
+      { "<leader>fR", LazyLite.pick("oldfiles", { cwd = vim.uv.cwd() }),                 desc = "Recent (cwd)" },
       -- git
-      { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
-      { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Status" },
+      { "<leader>gc", "<cmd>Telescope git_commits<CR>",                                  desc = "Commits" },
+      { "<leader>gs", "<cmd>Telescope git_status<CR>",                                   desc = "Status" },
       -- search
-      { '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
-      { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
-      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
-      { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-      { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-      { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
-      { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
-      { "<leader>sg", LazyLite.pick("live_grep"), desc = "Grep (Root Dir)" },
-      { "<leader>sG", LazyLite.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
-      { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
-      { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
-      { "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
-      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-      { "<leader>sl", "<cmd>Telescope loclist<cr>", desc = "Location List" },
-      { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
-      { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
-      { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-      { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
-      { "<leader>sq", "<cmd>Telescope quickfix<cr>", desc = "Quickfix List" },
-      { "<leader>sw", LazyLite.pick("grep_string", { word_match = "-w" }), desc = "Word (Root Dir)" },
+      { '<leader>s"', "<cmd>Telescope registers<cr>",                                    desc = "Registers" },
+      { "<leader>sa", "<cmd>Telescope autocommands<cr>",                                 desc = "Auto Commands" },
+      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>",                    desc = "Buffer" },
+      { "<leader>sc", "<cmd>Telescope command_history<cr>",                              desc = "Command History" },
+      { "<leader>sC", "<cmd>Telescope commands<cr>",                                     desc = "Commands" },
+      { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>",                          desc = "Document Diagnostics" },
+      { "<leader>sD", "<cmd>Telescope diagnostics<cr>",                                  desc = "Workspace Diagnostics" },
+      { "<leader>sg", LazyLite.pick("live_grep"),                                        desc = "Grep (Root Dir)" },
+      { "<leader>sG", LazyLite.pick("live_grep", { root = false }),                      desc = "Grep (cwd)" },
+      { "<leader>sh", "<cmd>Telescope help_tags<cr>",                                    desc = "Help Pages" },
+      { "<leader>sH", "<cmd>Telescope highlights<cr>",                                   desc = "Search Highlight Groups" },
+      { "<leader>sj", "<cmd>Telescope jumplist<cr>",                                     desc = "Jumplist" },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>",                                      desc = "Key Maps" },
+      { "<leader>sl", "<cmd>Telescope loclist<cr>",                                      desc = "Location List" },
+      { "<leader>sM", "<cmd>Telescope man_pages<cr>",                                    desc = "Man Pages" },
+      { "<leader>sm", "<cmd>Telescope marks<cr>",                                        desc = "Jump to Mark" },
+      { "<leader>so", "<cmd>Telescope vim_options<cr>",                                  desc = "Options" },
+      { "<leader>sR", "<cmd>Telescope resume<cr>",                                       desc = "Resume" },
+      { "<leader>sq", "<cmd>Telescope quickfix<cr>",                                     desc = "Quickfix List" },
+      { "<leader>sw", LazyLite.pick("grep_string", { word_match = "-w" }),               desc = "Word (Root Dir)" },
       { "<leader>sW", LazyLite.pick("grep_string", { root = false, word_match = "-w" }), desc = "Word (cwd)" },
-      { "<leader>sw", LazyLite.pick("grep_string"), mode = "v", desc = "Selection (Root Dir)" },
-      { "<leader>sW", LazyLite.pick("grep_string", { root = false }), mode = "v", desc = "Selection (cwd)" },
-      { "<leader>uC", LazyLite.pick("colorscheme", { enable_preview = true }), desc = "Colorscheme with Preview" },
+      { "<leader>sw", LazyLite.pick("grep_string"),                                      mode = "v",                       desc = "Selection (Root Dir)" },
+      { "<leader>sW", LazyLite.pick("grep_string", { root = false }),                    mode = "v",                       desc = "Selection (cwd)" },
+      { "<leader>uC", LazyLite.pick("colorscheme", { enable_preview = true }),           desc = "Colorscheme with Preview" },
       {
         "<leader>ss",
         function()
@@ -190,8 +191,11 @@ return {
 
       return {
         defaults = {
-          prompt_prefix = " ",
-          selection_caret = " ",
+          -- prompt_prefix = " ",
+          prompt_prefix = "   ",
+          -- selection_caret = " ",
+          selection_caret = " ",
+          selection_caret = " ",
           -- open files in the first window that is an actual file.
           -- use the current window if no other window is available.
           get_selection_window = function()
@@ -205,6 +209,20 @@ return {
             end
             return 0
           end,
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+              results_width = 0.8,
+            },
+            vertical = {
+              mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+          },
           mappings = {
             i = {
               ["<c-t>"] = open_with_trouble,
@@ -215,8 +233,15 @@ return {
               ["<C-Up>"] = actions.cycle_history_prev,
               ["<C-f>"] = actions.preview_scrolling_down,
               ["<C-b>"] = actions.preview_scrolling_up,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+              ['<C-u>'] = false,
+              -- Exit out of insert mode in telescope
+              ['<C-c>'] = { "<esc>", type = "command" },
             },
             n = {
+              -- Quit the telescope window
+              ['<C-c>'] = actions.close,
               ["q"] = actions.close,
             },
           },
@@ -293,9 +318,9 @@ return {
       local Keys = require("lazylite.plugins.lsp.keymaps").get()
       -- stylua: ignore
       vim.list_extend(Keys, {
-        { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition", has = "definition" },
-        { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References", nowait = true },
-        { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, desc = "Goto Implementation" },
+        { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end,      desc = "Goto Definition",       has = "definition" },
+        { "gr", "<cmd>Telescope lsp_references<cr>",                                                    desc = "References",            nowait = true },
+        { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end,  desc = "Goto Implementation" },
         { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto T[y]pe Definition" },
       })
     end,
